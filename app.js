20807ed6c30f0714abc9c1119021c8bd1f4389b5
nontas <tsakoto90@gmail.com>
2022-08-23 17:51:37 +0300

    newListItems

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
   var today = new Date();
@@ -21,13 +25,18 @@ app.get("/", function(req, res) {
 
 
 
-  res.render("list", {
-    kindOfDay: day
-  });
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 
+app.post("/", function(req, res){
+  var item =req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+});
 
 
 
