e9082ce674b2bcb4b28dbc01975b093e0c45183d
nontas <tsakoto90@gmail.com>
2022-08-25 17:10:15 +0300

    Adding CSS Stylsheets to Website

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
 
 
@@ -31,7 +32,7 @@ app.get("/", function(req, res) {
 
 
 app.post("/", function(req, res){
-  var item =req.body.newItem;
+  let item =req.body.newItem;
 
   items.push(item);
 
